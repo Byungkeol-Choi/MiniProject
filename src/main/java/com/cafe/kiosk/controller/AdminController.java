b5342d318838cc30b5979faa95617f57b1c8b736
Byung Keol Choi <byungkeol.choi@gmail.com>
2026-03-31 09:39:40 +0900

    import application-secret.yml 생성

--- src/main/java/com/cafe/kiosk/controller/AdminController.java
+++ src/main/java/com/cafe/kiosk/controller/AdminController.java
@@ -5,16 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
-//    테스트용
-    @GetMapping("/")
-    public String index() {
-//        return "/fragments/layout";
-        return "/kiosk/index";
-    }
 
     @GetMapping("/admin/login")
     public String adminLogin() {
-//        return "/fragments/layout";
                 return "/admin/login";
     }
 }
